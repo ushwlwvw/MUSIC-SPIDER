6ed4a91106186956ac863e16e522be73bd3d7f3c
ushwlwvw <126856143+ushwlwvw@users.noreply.github.com>
2023-03-16 21:04:35 +0300

    Update start.py

--- FallenMusic/Modules/start.py
+++ FallenMusic/Modules/start.py
@@ -38,7 +38,7 @@ async def fallen_st(_, message: Message):
         if len(message.text.split()) > 1:
             cmd = message.text.split(None, 1)[1]
             if cmd[0:3] == "inf":
-                m = await message.reply_text("⚡")
+                m = await message.reply_text("يتم البحث على المعلومات...")
                 query = (str(cmd)).replace("info_", "", 1)
                 query = f"https://www.youtube.com/watch?v={query}"
                 results = VideosSearch(query, limit=1)
